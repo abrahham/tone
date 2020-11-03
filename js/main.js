d8429bfabb974d5b65e8f04564da31ea7ea02190
= <valvdomero@gmail.com>
2020-11-02 23:42:43 -0600

    recorrdio de imágenes en lista

--- js/main.js
+++ js/main.js
@@ -3,6 +3,22 @@ var showcase = document.querySelector("#top-showcase");
 var lista = document.querySelectorAll("#top-lista > li");
 lista.forEach((item, index) => {
 	item.addEventListener('mouseover', () => {
-		showcase.style.backgroundImage = `url('img/${imagenes[index]}')`;
-	});			
+		cambiarImagen(index);
+	});
+	item.addEventListener('click', () => {
+		item.classList.add("seleccionado");
+	});
 });
+
+function recorrerLista() {
+	var index = 0;
+	setInterval(function() {
+		lista.forEach(item => item.classList.remove("seleccionado"));
+		lista[index].classList.add("seleccionado");
+		cambiarImagen(index);
+		index = (index == lista.length - 1) ? 0 : index + 1;
+	}, 900);
+}
+function cambiarImagen(index) {
+	showcase.style.backgroundImage = `url('img/${imagenes[index]}')`;
+}
